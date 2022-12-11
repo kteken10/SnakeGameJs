2510184250aa0b58ff5ace204340f25580d26359
dissang <dissangfrancis@yahoo.com>
2022-12-11 05:43:34 +0100

    Controle the snakeHead direction by {gauche,droite,bas,haut}

--- AirPlay.js
+++ AirPlay.js
@@ -4,7 +4,7 @@ const color = ['white', 'red', 'yellow','green'];
 var round =document.querySelector('#container');
 const Round={
     width:(window.innerWidth)*0.6,
-    height:(window.innerHeight)*3,
+    height:(window.innerHeight)*2.8,
     setOnscreen:roundSet=>{
         round.style.width=Round.width+'px';
         round.style.height=Round.height+'px';
@@ -12,8 +12,7 @@ const Round={
     getDimension:dimens=>{
         // dimens dimension depending of Object what we want to display
        if( dimens == "snake"){
-        return [(Round.width)/35, Round.height/60]
-
+        return [(Round.width)/35, Round.height/70]
        }    
     }
 }
@@ -29,9 +28,8 @@ const Chrono={
         chrono.style.marginLeft =  0+'px';
         chrono.style.marginTop  =  0+'px';
         var chrono_start = 0;
-        var chrono_end = 4;
-       
-        var myChrono = setInterval(() => { 
+        const chrono_end = 4;
+        const myChrono = setInterval(() => { 
             chrono.innerHTML  = '<h1 id="chronoNumber">' +   chrono_start+   '</h1>'; 
             let chronoNUmber = document.getElementById('chronoNumber');
             chronoNUmber.style.fontSize='43px';
@@ -44,7 +42,6 @@ const Chrono={
                 clearInterval(myChrono);
             }
             }, 1200);
-       
     }
 }
 
@@ -53,14 +50,13 @@ const Chrono={
 function   setColor(){
     const color = ['red','green','blue','yellow','purple','orange','pink'];
     choice_color = color [Math.round(Math.random()*color.length-1)]
-    console.log(choice_color);
     return choice_color;      
 }
 
-//INITIALIZE AND CREATE THE SNAKE
-
-
-
+//FUNCTION TO RETURN THE KEYBOARD KEY PRESSED:
+function getKeyboardPress(){
+   
+}
 
 class Snake{
     
@@ -68,26 +64,76 @@ class Snake{
         this.headSnake=[document.createElement('div')];
         round.appendChild(this.headSnake[0]);
         this.headSnake[0].id="snake";
-        const snake_parameter = {
-            headSnakePosition_x:Round.getDimension("snake")[0],
-            headSnakePosition_y:Round.getDimension("snake")[1]
-        }
+        this.direction ='gauche';
+        this.position_x = 400;
+        this.position_y = 238;
+        this.headSnake[0].style.left=this.position_x+'px';
+        this.headSnake[0].style.top=this.position_y+'px';
+       
     }
-
-    
-   
+    getDimension(){ 
+        const width = this.headSnake[0].style.width.replace('px', '');
+        const height = this.headSnake[0].style.height.replace('px', '');
+        return { width: width, height: height };
+    }
     setOnRound(){
         this.headSnake[0].style.backgroundColor = setColor();
         this.headSnake[0].style.width = Round.getDimension("snake")[0]+'px';
         this.headSnake[0].style.height = Round.getDimension("snake")[1]+'px';
-        this.headSnake[0].style.border = '1px solid blue';
-        
+        this.headSnake[0].style.position = 'relative';
+        this.headSnake[0].style.border = '1px solid #00e7ff';
     }
-    mooveSnake(){
-
+    moveToRound(){
+        
+        var myChrono = setInterval(() => { 
+            this.position_x = this.position_x;
+            this.position_y = this.position_y; 
+            switch (this.direction) {   
+                case 'gauche':
+                    if(this.position_x >= -400){
+                        this.position_x -= 20;
+                        if(this.position_x == -420){
+                            this.position_x = 400;
+                            this.headSnake[0].style.left = this.position_x+'px';
+                        }             
+                    }
+                       break;
+                case 'droite':
+                    if(this.position_x <= 400){
+                        this.position_x += 20;
+                        if(this.position_x == 420){
+                            this.position_x = -400;
+                            this.headSnake[0].style.left = this.position_x+'px';
+                        }                                              
+                    }
+                    break; 
+                case 'bas':
+                    if(this.position_y <= 240){
+                        this.position_y += 28;
+                        if(this.position_y >= 264){
+                            this.position_y = -238;
+                            this.headSnake[0].style.top = this.position_y+'px';
+                        }
+                    }
+                    break;  
+                case 'haut':
+                    if(this.position_y >= -240){
+                        this.position_y -= 28;
+                        if(this.position_y <= -264){
+                            this.position_y = 238;
+                            this.headSnake[0].style.top = this.position_y+'px';
+                        }
+                    }
+                    break;    
+                default:
+                    break;
+            }
+                this.headSnake[0].style.left = this.position_x+'px';
+                this.headSnake[0].style.top = this.position_y+'px';
+            }, 500);
     }
         snakeDetails(){
-            console.log(this.headSnake);
+            console.log(this.headSnake[0]);
         }
 }
 //INITIALIZE AND CREATE THE FOOD OF SNAKE
@@ -103,13 +149,12 @@ class Food{
         mooveSnake(){
 
         }
-        
-
-    
 }
 Round.setOnscreen();
 Chrono.setOnround();
 let snake = new Snake('red',1,2);
 snake.snakeDetails();
 snake.setOnRound();
+snake.moveToRound();
 // var chrono=new Chrono('chrono1',dimens.ledposition_x,dimens.ledposition_y);
+
